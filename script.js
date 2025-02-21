644d04eaf9d718dbaa283e0aa2f8eb53353f909c
darialberto <webdarihdl@gmail.com>
2025-02-20 18:21:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const canvas = new fabric.Canvas('canvas');
 
 // Cargar la imagen de la camiseta
-fabric.Image.fromURL('https://gdurl.com/gYUX', function (img) {
+fabric.Image.fromURL('https://via.placeholder.com/500x600', function (img) {
   img.scaleToWidth(500);
   canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas));
 });
@@ -26,4 +26,5 @@ function downloadImage() {
   link.download = 'camiseta-diseño.png';
   link.href = canvas.toDataURL({ format: 'png', quality: 1 });
   link.click();
+}
 }
